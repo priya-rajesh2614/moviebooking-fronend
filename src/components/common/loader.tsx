a3d461cc5b5a4ea6e23d30e4c75d2014bd177c2f
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-03-29 23:26:18 +0530

    add loader

---
+++ src/components/common/loader.tsx
@@ -0,0 +1,9 @@
+
+const Loader = () => {
+
+    return <div className="d-flex justify-content-center align-items-center vh-100">
+        <span className="loader"></span>
+    </div>
+}
+
+export default Loader
