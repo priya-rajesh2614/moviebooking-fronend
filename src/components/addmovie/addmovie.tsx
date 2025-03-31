c625342d873cea9ca0a57ebf6955936e5e09f891
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-03-31 17:54:40 +0530

    addmonie api wire up implementation

---
+++ src/components/addmovie/addmovie.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const AddMovie = () => {
+  const [movie, setMovie] = useState({
+    title: "",
+    genre: "",
+    language: "",
+    duration: "",
+    description: "",
+    imageUrl: "",
+  });
+  const navigate = useNavigate(); 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setMovie({ ...movie, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    await axios.post("http://localhost:8080/movies", movie);
+    alert("Movie added!");
+    navigate('/home')
+  };
+
+  return (
+    <div className="card  mt-4">
+      <h3>Add Movie</h3>
+      <input className="form-control my-2" name="title" placeholder="Title" onChange={handleChange} />
+      <input className="form-control my-2" name="genre" placeholder="Genre" onChange={handleChange} />
+      <input className="form-control my-2" name="language" placeholder="Language" onChange={handleChange} />
+      <input className="form-control my-2" name="duration" placeholder="Duration (min)" onChange={handleChange} />
+      <textarea className="form-control my-2" name="description" placeholder="Description" onChange={handleChange} />
+      <input className="form-control my-2" name="imageUrl" placeholder="Image URL" onChange={handleChange} />
+      <button className="btn1 mt-2" style={{width:"200px"}}  onClick={handleSubmit}>Add Movie</button>
+    </div>
+  );
+};
+
+export default AddMovie;
