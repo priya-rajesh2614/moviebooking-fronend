e078132a1ea3600527497cc63c2e2ba53ac6eb0f
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-03-31 17:54:40 +0530

    add the shows page and seat page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Home from './components/home/home'
 
 import './App.css'
 import TheaterList from './components/thearters/theaters'
+import Showtimes from './components/shows/showtimes'
+import Seats from './components/seats/seats'
 
 
 
@@ -17,6 +19,8 @@ function App() {
             <Route path='/register' element={<Register/>}/>
             <Route path='/home' element={<Home/>}/>
             <Route path='/movies/:movieId/theaters' element={<TheaterList/>}/>
+            <Route path='/movies/:movieId/theaters/:theaterId/shows' element={<Showtimes/>}/>
+            <Route path="/movies/:movieId/theaters/:theaterId/shows/:showId/seats" element={<Seats/>}/>
           </Routes>
       
         </BrowserRouter>
