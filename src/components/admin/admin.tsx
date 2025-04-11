164ae46267d17b931a3c3a83f35ebd83e84bb84e
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-04-11 10:22:42 +0530

    admin api wire up implementation

---
+++ src/components/admin/admin.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const AdminDashboard = () => {
+  return (
+    <div className=" card mt-5 text-center" style={{justifyContent: 'center',
+      display: 'flex',flexDirection:"column",width:"400px"}}>
+      <h2 className="mb-4">Admin Panel</h2>
+      <div className="d-grid gap-3" style={{
+    width: '200px',
+    justifyContent: 'center',
+    display: 'flex',
+    marginLeft: '65px'
+}}>
+        <Link to="/admin/add-movie" className="btn btn-primary">Add Movie</Link>
+        <Link to="/admin/add-theater" className="btn btn-success">Add Theater</Link>
+        <Link to="/admin/add-show" className="btn btn-warning">Add Show</Link>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
