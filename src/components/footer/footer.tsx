eed645d75f7b3a190a256b16bc24b5ca72381b73
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-04-11 10:28:28 +0530

    footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,87 @@
+export default function Footer(){
+    return(
+        <>
+<footer id="footer" className="footer-1">
+<div className="main-footer widgets-dark typo-light">
+<div className="container">
+<div className="row">
+  
+<div className="col-xs-12 col-sm-6 col-md-3">
+<div className="widget subscribe no-box">
+<h5 className="widget-title">BOOKITNOW<span></span></h5>
+
+<p>BookItNow is a user-friendly movie ticket booking platform where users can discover movies, select theaters, view showtimes, and reserve their favorite seats—all in a few simple steps.
+
+</p>
+</div>
+</div>
+
+<div className="col-xs-12 col-sm-6 col-md-3">
+<div className="widget no-box">
+<h5 className="widget-title">Quick Links<span></span></h5>
+<ul className="thumbnail-widget">
+<li>
+<div className="thumb-content"><a href="/home">Home</a></div> 
+</li>
+<li>
+<div className="thumb-content"><a href="/theaters">Theaters</a></div> 
+</li>
+<li>
+<div className="thumb-content"><a href="/shows">Shows</a></div> 
+</li>
+<li>
+<div className="thumb-content"><a href="/seats">Seats</a></div> 
+</li>
+<li>
+<div className="thumb-content"><a href="/payment">Payment</a></div>  
+</li>
+<li>
+<div className="thumb-content"><a href="/ticket">Ticket</a></div> 
+</li>
+
+</ul>
+</div>
+</div>
+
+<div className="col-xs-12 col-sm-6 col-md-3">
+<div className="widget no-box">
+<h5 className="widget-title">Help<span></span></h5>
+<p>🤝 We’re Here to Help!<br/>
+Have questions, feedback, or need support with your booking?
+We’d love to hear from you!<br/>
+
+</p>
+
+</div>
+</div>
+
+<div className="col-xs-12 col-sm-6 col-md-3">
+
+<div className="widget no-box">
+<a href="/home"><img
+              src="https://uploads.turbologo.com/uploads/design/preview_image/78373916/watermark_preview_image20250408-1-1lojkdi.png"
+              style={{ borderRadius: '20px', height: '150px', marginRight: '-1px' }}
+              alt="logo"
+            /></a>
+
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+  
+<div className="footer-copyright">
+<div className="container">
+<div className="row">
+<div className="col-md-12 text-center">
+<p>Copyright Company Name © 2022. All rights reserved.</p>
+</div>
+</div>
+</div>
+</div>
+</footer>
+</>
+)
+}
