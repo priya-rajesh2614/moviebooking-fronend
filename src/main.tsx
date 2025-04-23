8e2d23349eb8f8456264255639977f5fde27ef20
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-04-23 18:56:33 +0530

    i add router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
 
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+  <BrowserRouter>
+
     <App />
+    </BrowserRouter>
+
   </StrictMode>,
 )
