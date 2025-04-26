fdeaa4026679a69c910c5c8c45f7801366434831
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-04-26 15:09:04 +0530

    add navigate to home when i click

--- src/components/addshow/addshow.tsx
+++ src/components/addshow/addshow.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const AddShow = () => {
   const [show, setShow] = useState({ movieId: "", theaterId: "", showTime: "" });
@@ -18,6 +19,7 @@ const AddShow = () => {
       }
     }).then(res => setTheaters(res.data));
   }, []);
+  const navigate = useNavigate(); 
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     setShow({ ...show, [e.target.name]: e.target.value });
@@ -30,11 +32,12 @@ const AddShow = () => {
       }
     });
     alert("Show added!");
+    navigate('/home')
     
   };
 
   return (
-    <div className="card mt-4">
+    <div className="card card-width mt-4">
       <h3>Add Show</h3>
       <select className="form-select my-2" name="movieId" onChange={handleChange}>
         <option value="">Select Movie</option>
@@ -45,7 +48,7 @@ const AddShow = () => {
         {theaters.map((t: any) => <option key={t.id} value={t.id}>{t.name}</option>)}
       </select>
       <input className="form-control my-2" name="showTime" type="datetime-local" onChange={handleChange} />
-      <button className="btn1  mt-2" style={{width:"200px"}}  onClick={handleSubmit}>Add Show</button>
+      <button className="btn1  mt-2" style={{width:"338px"}}  onClick={handleSubmit}>Add Show</button>
     </div>
   );
 };
