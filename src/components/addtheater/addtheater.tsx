cc4745a139876c7b9bd4aeaeb40377cee7232d2d
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-04-26 15:09:21 +0530

    add navigate to home when i click

--- src/components/addtheater/addtheater.tsx
+++ src/components/addtheater/addtheater.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const AddTheater = () => {
   const [theater, setTheater] = useState({
@@ -11,19 +12,20 @@ const AddTheater = () => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTheater({ ...theater, [e.target.name]: e.target.value });
   };
-
+  const navigate = useNavigate(); 
   const handleSubmit = async () => {
     await axios.post("http://localhost:8080/api/admin/theaters", theater);
     alert("Theater added!");
+    navigate('/home')
   };
 
   return (
-    <div className="card mt-4">
+    <div className="card card-width mt-4">
       <h3>Add Theater</h3>
       <input className="form-control my-2" name="name" placeholder="Name" onChange={handleChange} />
       <input className="form-control my-2" name="location" placeholder="Location" onChange={handleChange} />
       <input className="form-control my-2" name="image_url" placeholder="Image URL" onChange={handleChange} />
-      <button className="btn1 mt-2" style={{width:"200px"}} onClick={handleSubmit}>Add Theater</button>
+      <button className="btn1 mt-2" style={{width:"338px"}} onClick={handleSubmit}>Add Theater</button>
     </div>
   );
 };
